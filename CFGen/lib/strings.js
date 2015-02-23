ddcdd74fda5fbac6ab453e233c75e39023114a92
Immanuel Hayden <immanuel.hayden@gmail.com>
2015-02-23 21:38:06 +0100

    get rid of lodash, add some more info to the package.json

--- CFGen/lib/strings.js
+++ CFGen/lib/strings.js
@@ -1,5 +1,4 @@
 ﻿var remove_diacritics = require('diacritics').remove;
-var ld = require('lodash');
 
 var transliterationMap = {
     "Ä": "AE",
@@ -10,12 +9,27 @@ var transliterationMap = {
     "ß": "SS"
 };
 
+// basic idea from lodash
+var repeat = function (string, n) {
+    var result = '';
+    
+    do {
+        if (n % 2) {
+            result += string;
+        }
+        n = Math.floor(n / 2);
+        string += string;
+    } while (n);
+    
+    return result;
+}
+
 module.exports.sanitize = function (input) {
     input = input.trim().toUpperCase().replace(' ', '');
     
-    ld.forEach(transliterationMap, function (val, key) {
+    for (var key in transliterationMap) {
         input = input.replace(key, transliterationMap[key]);
-    });
+    }
 
     return remove_diacritics(input);
 };
@@ -24,27 +38,28 @@ module.exports.separateConsonantsAndVowels = function(input) {
     input = input.toUpperCase();
     var allVowels = ['A', 'E', 'I', 'O', 'U'];
     var result = { consonants: "", vowels: "" };
-
-    ld.forEach(input, function(c) {
-        if (ld.includes(allVowels, c)) {
+    
+    for(var i = 0; i < input.length; i++ ) {
+        var c = input[i];
+        if (allVowels.indexOf(c) > -1) {
             result.vowels = result.vowels + c;
         } else {
             result.consonants = result.consonants + c;
         }
-    });
+    };
     return result;
 }
 
 module.exports.padLeft = function (string, padding, length) {
     var count = Math.ceil(length / padding.length);
-    padding = ld.repeat(padding, count);
+    padding = repeat(padding, count);
 
     return (padding + string.toString()).slice(-length);
 };
 
 module.exports.padRight = function (string, padding, length) {
     var count = Math.ceil(length / padding.length);
-    padding = ld.repeat(padding, count);
+    padding = repeat(padding, count);
     
     return (string.toString() + padding).slice(0, length);
 };
